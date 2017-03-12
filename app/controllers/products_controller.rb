3b153da6b68e9dee8b7eefa21edae74be57fbbaa
Robert Rossol <shyguyrbr1@gmail.com>
2017-03-12 10:29:43 -0500

    adds new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,22 @@
 class ProductsController < ApplicationController
-  def all_products
+  def index
     @products=Product.all
-    render "all_products.html.erb"
+    render "index.html.erb"
+  end
+
+  def show
+    product_id = params[:id]
+    @products = Product.find_by(id: product_id)
+    render "show.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    product = Product.new(name: params[:form_name], price: params[:form_price],image: params[:image_name], description: params[:description_name])
+    product.save
+    render "create.html.erb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get "/all_products.url" => "products#all_products"
+  get "/" => "products#index"
+  get "/products" => "products#index"
+  get "/products/new" => "products#new"
+  post "/products" => "products#create"
+  get "/products/:id" => "products#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
